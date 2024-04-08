40262b3f14a32911b413c6e60d31878f7028c12a
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-04-08 17:35:25 -0400

    changed to url

--- empty-vue/src/main.js
+++ empty-vue/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.21/vue.cjs.js'
 import App from './App.vue'
 
 createApp(App).mount('#app')
